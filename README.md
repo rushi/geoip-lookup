31bc2091534e3025d46986ecde8ea48c90d9f24b
Rushi Vishavadia <rushi.v@gmail.com>
2020-05-03 12:55:10 +0530

    First version

---
+++ README.md
@@ -0,0 +1,8 @@
+## geoip-lookup
+
+Quick tool to batch look up IPs in Maxmind's GeoIP Database
+
+## TODO
+
+* Setup instructions for installing GeoIP database
+* Configure GeoIP library
---
+++ app.js
@@ -0,0 +1,24 @@
+const argv = require('yargs').argv;
+const geoip = require('geoip-lite');
+const Table = require('cli-tableau');
+
+if (!argv._[0]) {
+    return console.error('Need one more IP address on the CLI');
+}
+
+var table = new Table({
+    head: ['IP', 'City', 'Region', 'Country', 'Timezone'],
+    borders: false
+});
+
+argv._.forEach(ip => {
+    const geo = geoip.lookup(ip);
+    if (geo) {
+        argv.v && console.log(geoip.pretty(ip), JSON.stringify(geo));
+        table.push([geoip.pretty(ip), geo.city, geo.region, geo.country, geo.timezone]);
+    } else {
+        table.push([ip, 'Invalid IP']);
+    }
+});
+
+console.log(table.toString());
